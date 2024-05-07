29ed1d91af478b90ecac0030c8c223df39c3fe60
kelvinfotech <kelvinfotech@gmail.com>
2024-05-07 10:20:01 +0300

    React Tdd Events Codealong

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,6 +1,15 @@
 import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event"; // Added import
 import App from "../App";
 
 import '@testing-library/jest-dom';
 
 // Code tests here
+test("checkbox appears as checked when user clicks it", () => {
+    render(<App />);
+  
+    const addPepperoni = screen.getByRole("checkbox", { name: /add pepperoni/i });
+  
+    userEvent.click(addPepperoni);
+    expect(addPepperoni).toBeChecked();
+  });
